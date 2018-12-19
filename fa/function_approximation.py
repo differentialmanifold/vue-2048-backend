fc94373df4c47ae043bb546bb502744ec36b7615
differentialmanifold <differentialmanifold@gmail.com>
2018-12-19 23:38:53 +0800

    add batchnormalization

--- fa/function_approximation.py
+++ fa/function_approximation.py
@@ -1,13 +1,12 @@
 import sys
 import itertools
-import os
+import random
 import time
 import argparse
 import numpy as np
 import keras
 from collections import namedtuple
-from keras.models import Sequential, load_model
-from keras.layers import Dense, Dropout, InputLayer, Flatten, Input, Lambda
+from keras.layers import Dense, Activation, Flatten, Input, Lambda, BatchNormalization
 from keras.optimizers import Adam
 
 if "../" not in sys.path:
@@ -37,8 +36,13 @@ class Estimator:
 
         main_input = Input(shape=(self.model_size, self.model_size), name='main_input')
         x = Flatten()(main_input)
-        x = Dense(256, activation='relu', name='first_layer')(x)
-        x = Dense(256, activation='relu', name='second_layer')(x)
+        # x = BatchNormalization()(x)
+        x = Dense(256, name='first_layer')(x)
+        # x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = Dense(256, name='second_layer')(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
         value_function_output = Dense(4, name='value_function_output')(x)
 
         action_input = Input(shape=(4,), name='action_input')
@@ -65,7 +69,7 @@ class Estimator:
     def save(self):
         self.model_save.save(self.model)
 
-    def predict(self, s):
+    def predict(self, s, train=0):
         """
         Predicts action values.
         Args:
@@ -74,12 +78,14 @@ class Estimator:
           Tensor of shape [batch_size, NUM_VALID_ACTIONS] containing the estimated
           action values.
         """
+
         main_input = self.model.get_layer('main_input').input
         value_function_output = self.model.get_layer('value_function_output').output
 
-        get_value_function = keras.backend.function(inputs=[main_input], outputs=[value_function_output])
+        get_value_function = keras.backend.function(inputs=[main_input, keras.backend.learning_phase()],
+                                                    outputs=[value_function_output])
 
-        return get_value_function([s])[0]
+        return get_value_function([s, train])[0]
 
     def predict_single(self, s):
         """
@@ -133,9 +139,11 @@ def make_epsilon_greedy_policy(estimator, epsilon, nA):
 
     def policy_fn(observation, available_direction):
         dir_arr = np.array(available_direction).astype(int)
+        dir_index = np.array([i for i in range(len(available_direction)) if available_direction[i]])
 
         A = (np.ones(nA, dtype=float) * dir_arr) * epsilon / np.sum(dir_arr)
-        best_action = np.argmax(np.array(estimator.predict_single(observation)) * dir_arr)
+        value_function = np.array(estimator.predict_single(observation))
+        best_action = dir_index[np.argmax(value_function[dir_index])]
         A[best_action] += (1.0 - epsilon)
         return A
 
@@ -144,8 +152,8 @@ def make_epsilon_greedy_policy(estimator, epsilon, nA):
 
 def q_learning(env,
                args,
-               replay_memory_size=50000,
-               replay_memory_init_size=5000,
+               replay_memory_size=500,
+               replay_memory_init_size=50,
                batch_size=32,
                discount_factor=1.0,
                alpha=0.5,
@@ -175,24 +183,22 @@ def q_learning(env,
     replay_memory = []
     Transition = namedtuple("Transition", ["state", "action", "reward", "next_state", "done"])
     # Populate the replay memory with initial experience
-    # print("Populating replay memory...")
-    # matrix, can_move_dir = env.reset()
-    # state = transferMatrixToState(matrix, env)
-    # for i in range(replay_memory_init_size):
-    #     action_probs = policy(state, can_move_dir)
-    #     action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
-    #     next_matrix, reward, done, _, _, next_can_move_dir = env.step(action)
-    #     next_state = transferMatrixToState(next_matrix, env)
-    #     replay_memory.append(Transition(state, action, reward, next_state, done))
-    #     if done:
-    #         matrix, can_move_dir = env.reset()
-    #         state = transferMatrixToState(matrix, env)
-    #     else:
-    #         state = next_state
-    #         can_move_dir = next_can_move_dir
-    # print("Init replay finished")
+    print("Populating replay memory...")
+    state, can_move_dir = env.reset()
+    for i in range(replay_memory_init_size):
+        action_probs = policy(state, can_move_dir)
+        action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
+        next_state, reward, done, _, _, next_can_move_dir = env.step(action)
+        replay_memory.append(Transition(state, action, reward, next_state, done))
+        if done:
+            state, can_move_dir = env.reset()
+        else:
+            state = next_state
+            can_move_dir = next_can_move_dir
+    print("Init replay finished")
 
     for i_episode in itertools.count(start=total_step):
+
         # Reset the environment and pick the first action
         state, can_move_dir = env.reset()
 
@@ -208,70 +214,68 @@ def q_learning(env,
 
             next_state, reward, done, _, _, next_can_move_dir = env.step(action)
 
-            # # If our replay memory is full, pop the first element
-            # if len(replay_memory) == replay_memory_size:
-            #     replay_memory.pop(0)
-            #
-            # # Save transition to replay memory
-            # replay_memory.append(Transition(state, action, reward, next_state, done))
-            #
-            # # TD Update
-            # # Sample a minibatch from the replay memory
-            # samples = random.sample(replay_memory, batch_size)
-            # for sample in samples:
-            #     best_next_action = np.argmax(Q[sample.next_state])
-            #     td_target = sample.reward + discount_factor * Q[sample.next_state][best_next_action]
-            #     td_delta = td_target - Q[sample.state][sample.action]
-            #     Q[sample.state][sample.action] += alpha * td_delta
+            # If our replay memory is full, pop the first element
+            if len(replay_memory) == replay_memory_size:
+                replay_memory.pop(0)
 
-            next_q_value = 0.0
-            if not done:
-                next_value_function = estimator.predict_single(next_state)
-                best_next_action = np.argmax(next_value_function)
-                next_q_value = next_value_function[best_next_action]
+            # Save transition to replay memory
+            replay_memory.append(Transition(state, action, reward, next_state, done))
 
-            td_target = reward + discount_factor * next_q_value
-            loss = estimator.update_single(state, action_array, td_target)
+            # TD Update
+            # Sample a minibatch from the replay memory
+            batch_state = []
+            batch_next_state = []
+            batch_action_array = []
+            batch_done = []
+            batch_reward = []
+            samples = random.sample(replay_memory, batch_size)
+            for sample in samples:
+                batch_state.append(sample.state)
+                action_array = np.zeros(shape=(4,))
+                action_array[sample.action] = 1
+                batch_action_array.append(action_array)
+                batch_next_state.append(sample.next_state)
+                batch_done.append(sample.done)
+                batch_reward.append(sample.reward)
 
-            # state_i = np.expand_dims(state, axis=0)
-            # a_i = np.expand_dims(action_array, axis=0)
-            # q_value = estimator.model.predict([state_i, a_i])
-            # print(
-            #     'in episode {} step {} loss is {} value function is {} q_value is {} action is {}'.format(i_episode, t,
-            #                                                                                               loss,
-            #                                                                                               estimator.predict_single(
-            #                                                                                                   state),
-            #                                                                                               q_value,
-            #                                                                                               action_array))
+            batch_state = np.array(batch_state)
+            batch_next_state = np.array(batch_next_state)
+            batch_action_array = np.array(batch_action_array)
+            batch_done = np.array(batch_done)
+            batch_reward = np.array(batch_reward)
+
+            batch_next_value_function = estimator.predict(batch_next_state)
+            batch_next_q_value = np.amax(batch_next_value_function, axis=1)
+            batch_next_q_value = batch_next_q_value * (batch_done.astype(float))
+
+            batch_target = batch_reward + discount_factor * batch_next_q_value
+
+            loss = estimator.update(batch_state, batch_action_array, batch_target)
+
+            # next_q_value = 0.0
+            # if not done:
+            #     next_value_function = estimator.predict_single(next_state)
+            #     best_next_action = np.argmax(next_value_function)
+            #     next_q_value = next_value_function[best_next_action]
             #
-            # print('first layer weights is:')
-            # weights_1 = estimator.model.get_layer('first_layer').get_weights()[0]
-            # print(weights_1.shape)
-            # print(np.sum(weights_1))
-            # print(weights_1[0][:5])
-            # print('second layer weights is:')
-            # weights_2 = estimator.model.get_layer('second_layer').get_weights()[0]
-            # print(weights_2.shape)
-            # print(np.sum(weights_2))
-            # print(weights_2[0][:5])
-            # print('value function layer weights is:')
-            # value_function_output_layer = estimator.model.get_layer('value_function_output')
-            # weights_3 = value_function_output_layer.get_weights()[0]
-            # print(weights_3.shape)
-            # print(np.sum(weights_3))
-            # print(weights_3[0][:5])
+            # td_target = reward + discount_factor * next_q_value
+            # loss = estimator.update_single(state, action_array, td_target)
+
 
             state = next_state
             can_move_dir = next_can_move_dir
 
             if done:
+                tensorBoardPlot.add_value('episode_len', t, i_episode)
+                tensorBoardPlot.add_value('episode_max', np.max(next_state), i_episode)
+                tensorBoardPlot.add_value('episode_loss', loss, i_episode)
                 break
 
         # Print out which episode we're on, useful for debugging.
         if (i_episode + 1) % int(args['outputInterval']) == 0:
             print('----------')
             print("Episode {}.".format(i_episode + 1))
-            test_Q(tensorBoardPlot, env, estimator, i_episode)
+            test_Q(tensorBoardPlot, estimator, i_episode)
 
         if (i_episode + 1) % (int(args['outputInterval']) * 10) == 0:
             print('start saving')
@@ -280,7 +284,7 @@ def q_learning(env,
             print('save cost {} second'.format(time.time() - current_time))
 
 
-def test_Q(tensorBoardPlot, env, estimator, step):
+def test_Q(tensorBoardPlot, estimator, step):
     board_without_tiles = BoardForTrain(size=int(args['size']))
     print('args size is {}'.format(args['size']))
 
